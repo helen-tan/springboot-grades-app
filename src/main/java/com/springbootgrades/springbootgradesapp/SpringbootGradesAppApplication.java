8d25e3715ef360b751cdedecf6a03942a070d185
helen-tan <helentanmz0909@gmail.com>
2022-12-13 17:11:30 +0800

    new Spring Boot project

---
+++ src/main/java/com/springbootgrades/springbootgradesapp/SpringbootGradesAppApplication.java
@@ -0,0 +1,13 @@
+package com.springbootgrades.springbootgradesapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootGradesAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootGradesAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springbootgrades/springbootgradesapp/SpringbootGradesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbootgrades.springbootgradesapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootGradesAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
