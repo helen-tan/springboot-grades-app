abaeb04e862105c8eb51f8efbb7164f2162d93f3
helen-tan <helentanmz0909@gmail.com>
2022-12-14 15:01:03 +0800

    Saved grade objects in an array + Used thmyeleaf loop to display grade objectgs

--- src/main/java/com/springbootgrades/springbootgradesapp/GradeController.java
+++ src/main/java/com/springbootgrades/springbootgradesapp/GradeController.java
@@ -1,20 +1,33 @@
 package com.springbootgrades.springbootgradesapp;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class GradeController {
+    // List of grade objects (only populated when the controller is first initialized)
+    List<Grade> studentGrades = Arrays.asList(
+        new Grade("Harry", "Potions", "C-"),
+        new Grade("Hermoine", "Arithmancy", "A+"),
+        new Grade("Neville", "Charms", "A-")
+    );
     
-    @GetMapping("/grades")
     // Returns the grades html (will find the view from the templates folder)
     // Model is directly accessed from the handler method's parameters
+    @GetMapping("/grades")
     public String getGrades(Model model) {
-        // Create Grade object (POJO object)
-        Grade grade =new Grade("Harry", "Potions", "C-");
+        // Create Grade object (POJO object) + add to list
+        // studentGrades.add(new Grade("Harry", "Potions", "C-"));
+        // studentGrades.add(new Grade("Hermoine", "Arithmancy", "A+"));
+        // studentGrades.add(new Grade("Neville", "Charms", "A-"));
+       
         // Store data in model
-        model.addAttribute("grade", grade);
+        model.addAttribute("grades", studentGrades);
         return "grades";
     }
 }
