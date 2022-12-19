1528a5ac0e91959a87b2f3c05bd4797c02ad66da
helen-tan <helentanmz0909@gmail.com>
2022-12-19 15:38:03 +0800

    Integration testing: AutoConfigureMockMvc

--- src/test/java/com/springbootgrades/springbootgradesapp/SpringbootGradesAppApplicationTests.java
+++ src/test/java/com/springbootgrades/springbootgradesapp/SpringbootGradesAppApplicationTests.java
@@ -1,13 +1,33 @@
 package com.springbootgrades.springbootgradesapp;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.springbootgrades.springbootgradesapp.controller.GradeController;
 
 @SpringBootTest
+@AutoConfigureMockMvc // Will register the MockMvc bean inside the Spring Container, which means it then can be injected
 class SpringbootGradesAppApplicationTests {
 
+	// Autowire/dependency inject GradeController bean
+	@Autowired
+	private GradeController controller; // don't really need to do this
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	// Load Spring Context/ Spring Container. contextLoads is a lifesycle method
 	@Test
 	void contextLoads() {
+		// verify that controller is not null (was the bean actually injected into the variable?)
+		assertNotNull(controller); 
+		// verify that MockMvc bean is not null
+		assertNotNull(mockMvc);
 	}
 
 }
